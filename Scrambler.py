a61e51ff5a6e095701381af6481849c45837064a
Eryk J <infiniti@inventati.org>
2022-05-19 21:16:20 -0400

    Switch to re.sub instead of replace

--- Scrambler.py
+++ Scrambler.py
@@ -26,36 +26,34 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-VERSION = 'v0.0.1'
+VERSION = 'v0.0.2'
 
 
 import random, argparse, re, string
 
 
-def scramble(word):
-    if len(word) < 3:
-        return word
-    inner = list(word[1:-1])
-    random.shuffle(inner)
-    return word[0] + ''.join(inner) + word[-1]
+def chunk(m, str):
 
-def chunk(str):
-    s = re.compile(r'\s')
-    words = s.split(u''.join(c for c in str if c not in set(string.punctuation)))
-    for word in words:
-        wrod = scramble(word)
-        str = str.replace(word, wrod, 1)
-    return str
+    def scramble(match):
+        word = match.group(1)
+        inner = list(word[1:-1])
+        random.shuffle(inner)
+        return word[0] + ''.join(inner) + word[-1]
+
+    return re.sub(m, scramble, str)
 
 def main(args):
     print(f"\n*** {APP} {VERSION} ***")
     if args['Infile'] == args['Outfile']:
         print(f"\nPlease specify an output file that is different than the input file\n")
     else:
+        m = re.compile(u'(\w{4,})')
+        outf = open(args['Outfile'], 'w', encoding="UTF-8")
         with open(args['Infile'], 'r', encoding="UTF-8") as inf:
-            with open(args['Outfile'], 'w', encoding="UTF-8") as outf:
-                txt = inf.readline()
-                outf.write(chunk(txt))
+            txt = inf.readlines()
+            for line in txt:
+                outf.write(chunk(m, line))
+        outf.close()
         print("\nCompleted successfully\n")
 
 
